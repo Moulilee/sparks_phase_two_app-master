3a4512a2d60efbe529353f59af59432e722d4ef8
moulilee <mouli.lee.1@gmail.com>
2019-07-23 18:39:02 +0530

    Initial commit

---
+++ app/src/main/java/com/example/akash/phasetwoproject/fragment/ProfessionalDetailsFragment.java
@@ -0,0 +1,19 @@
+package com.example.akash.phasetwoproject.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.akash.phasetwoproject.R;
+
+public class ProfessionalDetailsFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_professional_details,container,false);
+    }
+}
